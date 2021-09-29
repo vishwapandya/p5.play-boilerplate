cf76303eb2291314dc0214a3a05074cc49744549
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-09-29 17:48:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
+var movingRect;
+var fixedRect;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  fixedRect = createSprite(200,200,50,80);
+  fixedRect.shapeColor = "blue";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400,300,50,80);
+  movingRect.shapeColor = "blue"
+  movingRect.debug = true;
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2){
+    movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green";
+  } else{
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+
   drawSprites();
 }
